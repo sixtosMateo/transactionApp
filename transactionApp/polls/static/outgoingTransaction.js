562538927cc2de2686dfbd8e3d98f884a4906e84
mateosixtos <mateo@nlightntech.com>
2018-07-13 17:05:07 -0700

    hide/show itemNotFoundMessage

--- transactionApp/polls/static/outgoingTransaction.js
+++ transactionApp/polls/static/outgoingTransaction.js
@@ -1,46 +1,50 @@
+$('#itemNotFound').hide()
+$( "#cancel" ).click(function() {
+  localStorage.clear();
+  window.alert( "localStorage was clear" );
+  location.reload();
+});
+
+
 $("#id").change(function(){
 
   $(function(){
     var $input = $("#id").val();
     var $itemId = $('#itemId');
     var $itemName = $('#itemName');
-    var $itemDetails = $('#itemsList');
+    // var $itemDetails = $('#itemsList');
     var $itemQty= $('#itemQty');
     var $price = $('#price');
 
-
+    // if the input id doesnt exist in local storage
     if(localStorage.getItem($input) == null){
       var $itemContainer = [];
+
+      // make ajax get request to look for item
       $.ajax({
         type: 'GET',
         url:'/polls/items/',
         success:function(items){
 
-
           $.each(items, function(i,item){
-
+            // if the id from the item is the same as the input
             if(item.itemId == $input){
               var itemId = item.itemId;
               var itemName = item.name;
               var itemSalePrice = item.salePrice;
               var itemQty = 1;
+              // push all items as json object into $itemContainer array
               $itemContainer.push({"itemId": itemId, "itemName": itemName, "itemSalePrice": itemSalePrice, "itemQty": itemQty});
+              // set the item into localStorage as json object
               localStorage.setItem(itemId, JSON.stringify($itemContainer));
-              // $itemDetails.show();
-              // $itemNotFound.hide();
-              // $count++;
               return;
           }
           });
-          // if($count==0 ){
-          //   $itemDetails.hide();
-          //   $itemNotFound.show();
-          //
-          // }
         }
       });
 
     }
+    // parse the json object from localStorage based on the input
     var x = JSON.parse(localStorage.getItem($input));
     displayItemScanned(x);
 
@@ -48,21 +52,19 @@ $("#id").change(function(){
   });
 });
 
-
+// function displays the item into
 function displayItemScanned(object){
   var $itemDetails = $('#itemsList');
+  $('#itemNotFound').hide()
   if(object){
-
     object.forEach(function(key){
       $itemDetails.append("<dt> Item Id: "+key.itemId+ " Item Name: "+key.itemName+ " Sale Price " +key.itemSalePrice+" Qty: "+key.itemQty +"</dt>");
     });
+    window.alert(object[0].itemName);
+  }
+  else{
+    $('#itemNotFound').show();
   }
 
 
 }
-
-$( "#cancel" ).click(function() {
-  localStorage.clear();
-  window.alert( "localStorage was clear" );
-  location.reload();
-});
