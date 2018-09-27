6711f3a2760331ff4f65e14121e599ceee7aba63
mateosixtos <mateo@nlightntech.com>
2018-09-26 23:14:23 -0700

    outgoingtrasantiontable with content based on employee

--- transactionApp/polls/static/templateJS/employeeProfile.js
+++ transactionApp/polls/static/templateJS/employeeProfile.js
@@ -20,22 +20,31 @@ $("#employees-table").on("click", ".js-delete-employee", loadForm);
 });
 
 function calllbackTransaction(callback , url){
-  return $.ajax({
+  var id = $("#employeeId").val();
+  $.ajax({
          type: 'GET',
          url: url,
          success:function(transactions){
            $.each(transactions, function(i,transaction){
              // if the id from the item is the same as the input
                // window.alert(item.itemId);
-               callback(transaction);
+               if(transaction.employeeId == id ){
+                  callback(transaction);
+               }
            });
          }
        });
 }
 
-function displayTransaction(trasanction){
+function displayTransaction(transaction){
+  var tableName = $("#itemsTable");
+  tableName.append("<tr id=" + transaction.transactionId + ">" +
+                    "<td id='transactionId' value='"+transaction.transactionId+"'>"+transaction.transactionId+"</td>"+
+                    "<td id='tax' value='"+transaction.tax+"'>"+transaction.tax+"</td>"+
+                    "<td id='subtotal' value='"+transaction.subtotal+"'>"+transaction.subtotal+"</td>"+
+                    "<td id='total' value='"+transaction.total+"'>"+transaction.total+"</td>"+
+                    "</tr>");
 
-  console.log(trasanction);
 
 }
 
@@ -44,6 +53,7 @@ function displayTransaction(trasanction){
 $( "#outgoing-transaction" ).click(function() {
     window.alert("out");
     calllbackTransaction(displayTransaction, '/polls/api/outgoingTransactions/');
+
 });
 
 $( "#incoming-transaction" ).click(function() {
